a2a330d13933ec5e246b1c6079c2819075b0f5d1
kjiyuDEV <154062214+kjiyuDEV@users.noreply.github.com>
2024-11-14 18:22:44 +0900

    CORS 중복호출

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ if (process.env.NODE_ENV === 'production') {
         credentials: true,
     })
 );
-app.use(cors({ origin: true, credentials: true }));
+// app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: false }));
 app.use(express.json());
