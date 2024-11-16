e97f1fd0f290463c866223726b133b163a7c329f
kjiyuDEV <kjiyu1379@gmail.com>
2024-11-16 23:33:03 +0900

    cors 이슈

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ const app = express();
 
 // 환경변수 확인
 console.log('환경변수 확인:', {
-  MONGO_URI: process.env.MONGO_URI,
-  PORT: process.env.PORT,
+    MONGO_URI: process.env.MONGO_URI,
+    PORT: process.env.PORT,
 });
 const prod = process.env.NODE_ENV === 'production';
 console.log(prod, process.env.NODE_ENV === 'production', 'prod check in koyeb');
@@ -27,57 +27,49 @@ app.use(hpp());
 app.use(helmet({ contentSecurityPolicy: false }));
 
 // CORS 설정
-const whitelist = [
-  'http://localhost:8080',
-  'http://localhost:8081',
-  'https://kjiyu-devlog.com',
-  'https://www.kjiyu-devlog.com',
-  'https://kjiyudev.github.io',
-];
+const whitelist = ['http://localhost:3000', 'http://localhost:8080', 'http://localhost:8081', 'https://kjiyu-devlog.com', 'https://www.kjiyu-devlog.com', 'https://kjiyudev.github.io'];
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not Allowed Origin!')); // CORS 비허용
-    }
-  },
-  credentials: true,
+    origin: function (origin, callback) {
+        if (!origin || whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not Allowed Origin!')); // CORS 비허용
+        }
+    },
+    credentials: true,
 };
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(cors(corsOptions)); // CORS 옵션 적용
-  app.options('*', cors()); // 모든 경로에 대해 OPTIONS 요청 처리
-}
+app.use(cors(corsOptions));
+app.options('*', cors());
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: false }));
 if (process.env.NODE_ENV === 'production') {
-  app.use(morgan('combined'));
+    app.use(morgan('combined'));
 } else {
-  app.use(morgan('dev'));
+    app.use(morgan('dev'));
 }
 
 app.get('/', (req, res) => {
-  res.json({ message: '서버가 정상적으로 실행중입니다.' });
+    res.json({ message: '서버가 정상적으로 실행중입니다.' });
 });
 
 mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connecting Success!!'))
-  .catch((e) => console.log(e));
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB connecting Success!!'))
+    .catch((e) => console.log(e));
 
 // Use routes
 if (prod) {
-  app.all('*', (req, res, next) => {
-    const protocol = req.headers['x-forwarded-proto'] || req.protocol;
-    if (protocol !== 'https') {
-      res.redirect(`https://${req.hostname}${req.url}`);
-    } else {
-      next();
-    }
-  });
+    app.all('*', (req, res, next) => {
+        const protocol = req.headers['x-forwarded-proto'] || req.protocol;
+        if (protocol !== 'https') {
+            res.redirect(`https://${req.hostname}${req.url}`);
+        } else {
+            next();
+        }
+    });
 }
 
 app.use('/api/post', postRoutes);
@@ -85,9 +77,6 @@ app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/visitor', visitorRoutes);
 
-if (!prod)
-  app.listen(process.env.PORT, () =>
-    console.log(`Server running on port ${process.env.PORT}`)
-  );
+if (!prod) app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
 
 export default app;
