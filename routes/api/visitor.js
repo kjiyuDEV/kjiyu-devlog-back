55e6ace4a75f1c20057537743a40313a08e7d039
kjiyuDEV <kjiyu1379@gmail.com>
2024-11-23 14:33:04 +0900

    visitor 수정

--- routes/api/visitor.js
+++ routes/api/visitor.js
@@ -4,11 +4,14 @@ import Visitor from '../../models/visitor.js';
 const router = express.Router();
 
 // * 방문자
-router.get('/visit', async (req, res, next) => {
+router.post('/visit', async (req, res, next) => {
     try {
         const visit = await Visitor.findOne();
-        visit.views += 1;
-        visit.save();
+        if (req.body.type === 'up') {
+            visit.views += 1;
+            visit.save();
+        }
+
         res.json(visit);
     } catch (e) {
         console.error(e);
